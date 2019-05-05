fd8132df5940d25ec8cab740485ce4b81e5527af
Paullo144 <49080672+Paullo144@users.noreply.github.com>
2019-05-04 22:54:41 -0300

    Atualizando arquivos

---
+++ projeto_pi/src/projeto_pi/controller/ListarProfessoresController.java
@@ -0,0 +1,61 @@
+package controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import service.ProfessorService;
+import model.Professor;
+
+/**
+ * Servlet implementation class ListarProfessoresController
+ */
+@WebServlet("/listar_professores.do")
+public class ListarProfessoresController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		String chave = request.getParameter("data[search]");
+		String acao = request.getParameter("acao");
+		ProfessorService ps = new ProfessorService();
+		ArrayList<Professor> lista = null;
+		HttpSession session = request.getSession();
+		if (acao.equals("buscar")) {
+			if (chave != null && chave.length() > 0) {
+				lista = ps.carregar(Integer.parseInt(chave));
+			} else {
+				lista = ps.carregar();
+			}
+			session.setAttribute("lista", lista);
+		} else if (acao.equals("reiniciar")) {
+			session.setAttribute("lista", null);
+		}
+
+		RequestDispatcher dispatcher = request
+				.getRequestDispatcher("ListarProfessores.jsp");
+		dispatcher.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
--- projeto_pi/src/projeto_pi/controller/ManterProfessorController.java
+++ projeto_pi/src/projeto_pi/controller/ManterProfessorController.java
@@ -1,12 +1,15 @@
 package controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import model.Professor;
 import service.ProfessorService;
@@ -18,47 +21,99 @@ import service.ProfessorService;
 public class ManterProfessorController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * Default constructor. 
-     */
-    public ManterProfessorController() {
-        // TODO Auto-generated constructor stub
-    }
-
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		doPost(request, response);
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		Professor professor = new Professor();
-		ProfessorService professorService = new ProfessorService();
-		
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		String pAcao = request.getParameter("acao");
+		String pId = request.getParameter("id");
 		String pNome = request.getParameter("nome");
+		String pMatricula = request.getParameter("matricula");
 		String pEmail = request.getParameter("email");
 		String pSenha = request.getParameter("senha");
-		int pAdmin = Integer.parseInt(request.getParameter("administrador"));
-		String pMatricula = request.getParameter("matricula");
-		
+		int id = -1;
+		try {
+			id = Integer.parseInt(pId);
+		} catch (NumberFormatException e) {
+
+		}
+
+		Professor professor = new Professor();
+		professor.setIdProf(id);
 		professor.setNome(pNome);
+		professor.setMatricula(pMatricula);
 		professor.setEmail(pEmail);
 		professor.setSenha(pSenha);
-		professor.setAdm(pAdmin);
-		professor.setMatricula(pMatricula);
 		
-		professorService.criar(professor);
+		ProfessorService ps = new ProfessorService();
+		RequestDispatcher view = null;
+		HttpSession session = request.getSession();
 		
-		request.setAttribute("professor", professor);
-		RequestDispatcher view =
-		request.getRequestDispatcher("Professor.jsp");
+		if (pAcao.equals("Criar")) {
+			ps.criar(professor);
+			ArrayList<Professor> lista = new ArrayList<>();
+			lista.add(professor);
+			session.setAttribute("lista", lista);
+			view = request.getRequestDispatcher("ListarProfessores.jsp");
+		} else if (pAcao.equals("Excluir")) {
+			ps.excluir(professor);
+			ArrayList<Professor> lista = (ArrayList<Professor>)session.getAttribute("lista");
+			lista.remove(busca(professor, lista));
+			session.setAttribute("lista", lista);
+			view = request.getRequestDispatcher("ListarProfessores.jsp");			
+		} else if (pAcao.equals("Alterar")) {
+			ps.atualizar(professor);
+			ArrayList<Professor> lista = (ArrayList<Professor>)session.getAttribute("lista");
+			int pos = busca(professor, lista);
+			lista.remove(pos);
+			lista.add(pos, professor);
+			session.setAttribute("lista", lista);
+			request.setAttribute("professor", professor);
+			view = request.getRequestDispatcher("VisualizarProfessor.jsp");			
+		} else if (pAcao.equals("Visualizar")) {
+			professor = ps.carregarP(professor.getIdProf());
+			request.setAttribute("professor", professor);
+			view = request.getRequestDispatcher("VisualizarProfessor.jsp");		
+		} else if (pAcao.equals("Editar")) {
+			professor = ps.carregarP(professor.getIdProf());
+			request.setAttribute("professor", professor);
+			view = request.getRequestDispatcher("AlterarProfessor.jsp");		
+		} else if (pAcao.equals("Login")) {
+			ArrayList<Professor> lista = ps.carregar();
+			for(Professor p : lista) {
+				if(professor.getMatricula() == p.getMatricula() && professor.getSenha() == p.getSenha()) {
+					professor = ps.carregarP(p.getIdProf());
+					session.setAttribute("logged", professor);
+				}
+				view = request.getRequestDispatcher("Logado.jsp");
+			}
+		}
+		
 		view.forward(request, response);
-		
+
+	}
+
+	public int busca(Professor professor, ArrayList<Professor> lista) {
+		Professor to;
+		for(int i = 0; i < lista.size(); i++){
+			to = lista.get(i);
+			if(to.getIdProf() == professor.getIdProf()){
+				return i;
+			}
+		}
+		return -1;
 	}
 
 }
